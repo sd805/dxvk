72fa354acb33642b428cb7eba3b9f381a5a5cef6
Joshua Ashton <joshua@froggi.es>
2022-04-19 18:11:19 +0100

    [util] Implement string helpers on non-Windows platforms

--- src/util/util_string.cpp
+++ src/util/util_string.cpp
@@ -1,7 +1,11 @@
 #include "util_string.h"
 
+#include <codecvt>
+#include <locale>
+
 namespace dxvk::str {
   std::string fromws(const WCHAR *ws) {
+#ifdef _WIN32
     size_t len = ::WideCharToMultiByte(CP_UTF8,
       0, ws, -1, nullptr, 0, nullptr, nullptr);
 
@@ -15,16 +19,26 @@ namespace dxvk::str {
     ::WideCharToMultiByte(CP_UTF8, 0, ws, -1,
       &result.at(0), len, nullptr, nullptr);
     return result;
+#else
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.to_bytes(ws);
+#endif
   }
 
 
   void tows(const char* mbs, WCHAR* wcs, size_t wcsLen) {
+#ifdef _WIN32
     ::MultiByteToWideChar(
       CP_UTF8, 0, mbs, -1,
       wcs, wcsLen);
+#else
+    std::wstring result = tows(mbs);
+    wcslcpy(wcs, result.c_str(), wcsLen);
+#endif
   }
 
   std::wstring tows(const char* mbs) {
+#ifdef _WIN32
     size_t len = ::MultiByteToWideChar(CP_UTF8,
       0, mbs, -1, nullptr, 0);
     
@@ -38,6 +52,10 @@ namespace dxvk::str {
     ::MultiByteToWideChar(CP_UTF8, 0, mbs, -1,
       &result.at(0), len);
     return result;
+#else
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.from_bytes(mbs);
+#endif
   }
 
 }
